d068f6051cb3b9b0205a2c13a97a89cf9c4e20d4
Samuel Haugland <samhaug@apsalus>
2017-01-18 12:50:01 -0500

    consolidate

--- perturb_prem.py
+++ perturb_prem.py
@@ -6,15 +6,15 @@ def write_output(prem_array,title):
     with open(title+'.tvel','a') as f:
         f.write('prem_5km.tvel P\n')
         f.write('prem_5km.tvel S\n')
-        np.savetxt(f,prem_array)
+        np.savetxt(f,prem_array,fmt='%.3f')
 
 
-perturb = np.round(np.linspace(-0.03,0.03,num=12),2)
+perturb = np.round(np.linspace(-0.3,0.3,num=20),3)
 print len(perturb)
 
 for ii in perturb:
     prem = np.genfromtxt('./models/prem.tvel',skip_header=2)
-    prem[0:40,:] *= 1+ii
+    prem[2:40,1::] *= 1+ii
     write_output(prem,'prem_'+str(ii))
 
 
---
+++ reverb_traveltimes.py
@@ -0,0 +1,53 @@
+#!/home/samhaug/anaconda2/bin/python
+
+import obspy
+from obspy.taup import TauPyModel
+import numpy as np
+from glob import glob
+
+
+def reverb_times(mod):
+    '''
+    Find reverberation traveltimes from 1D model with taup
+    '''
+    model_in = mod.split('.tvel')[0]
+    model = TauPyModel(model=model_in)
+    phase_list = [
+                  'ScSScS',
+                  'sScSScS',
+                  'ScSScSScS',
+                  'sScSScSScS'
+                  ]
+    arrivals = model.get_travel_times(source_depth_in_km=160.,
+                                   distance_in_degree=50.,
+                                   phase_list = phase_list)
+    time_array = np.zeros(5)
+    time_array[0] = model_in.split('_')[1]
+
+    for ii in range(0,len(arrivals)):
+        time_array[1+ii]= np.round(arrivals[ii].time,1)
+
+    return time_array
+
+def diff_times(prem_dict,perturb_dict):
+    '''
+    Get traveltime delay between prem and perturbed model
+    '''
+    diff_dict = {}
+    for key in prem_dict.keys():
+        del_t = prem_dict[key] - perturb_dict[key]
+        diff_dict[key] = del_t
+
+    return diff_dict
+
+tvel_list = glob('tvel/*.tvel')
+prem_dict = reverb_times('prem')
+
+for mod in tvel_list:
+    print reverb_times(model)
+
+
+
+
+
+
