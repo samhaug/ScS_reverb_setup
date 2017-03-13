299981af887084d5ec2d2c0fbb585387f094b1de
Samuel Haugland <samhaug@umich.edu>
2017-03-13 18:30:45 -0400

    fixed mineos iasp model

--- models/make_iasp91.py
+++ models/make_iasp91.py
@@ -7,7 +7,7 @@ File Name : make_iasp91.py
 Purpose : Make iasp91 input file for mineos to use. Needs prem_noocean.txt
           as well as a csv IASP91 file from IRIS
 Creation Date : 13-03-2017
-Last Modified : Mon 13 Mar 2017 02:28:27 PM EDT
+Last Modified : Mon 13 Mar 2017 06:26:07 PM EDT
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -23,42 +23,55 @@ import seispy
 import scipy
 
 def main():
-    prad,prho,pmid,pend = read_prem('prem_noocean.txt')
+    meta,cores,mantle = read_prem('prem_noocean.txt')
     irad,ivp,ivs = read_iasp91('IASP91.csv')
-    pvp,pvs = interp_iasp(prad,irad,ivp,ivs)
-    inarray = np.hstack((
-              np.array([prad]).T,
-              np.array([prho]).T,
-              np.array([pvp]).T,
-              np.array([pvs]).T,
-              pmid,
-              np.array([pvp]).T,
-              np.array([pvs]).T,
-              np.array([pend]).T
+    imid,iend,irho = interp_iasp(irad,mantle[:,0],mantle[:,4:6],mantle[:,-1],mantle[:,1])
+    imantle = np.hstack((
+              np.array([irad]).T,
+              np.array([irho]).T,
+              np.array([ivp]).T,
+              np.array([ivs]).T,
+              imid,
+              np.array([ivp]).T,
+              np.array([ivs]).T,
+              np.array([iend]).T
               ))
+
+    inarray = np.vstack((cores,np.flipud(imantle)))
     write_model(inarray,'iasp91.txt')
 
 def read_prem(infile):
-    a = np.genfromtxt(infile,skip_header=3)
-    rad = a[:,0]
-    rho = a[:,1]
-    mid = a[:,4:6]
-    end = a[:,-1]
-    return rad,rho,mid,end
+    b = np.genfromtxt(infile,skip_header=3)
+    rad = b[:,0]
+    rho = b[:,1]
+    mid = b[:,4:6]
+    a = open(infile)
+    f = a.read()
+    meta = f.strip().split('\n')[2].split()
+    knots,nic,noc = meta[0],meta[1],meta[2]
+    a.close()
+    model = np.genfromtxt(infile,skip_header=3)
+    cores = model[0:int(noc),:]
+    mantle = model[int(noc)::,:]
+    return meta,cores,mantle
 
 def read_iasp91(infile):
     a = np.genfromtxt(infile,delimiter=',')
-    rad = a[:,1]*1000
-    vp = a[:,2]*1000
-    vs = a[:,3]*1000
-    return rad[::-1],vp,vs
+    mantle = a[0:137,:]
+    mantle[-1,1] = 3480.
+    irad = mantle[:,1]*1000
+    ivp = mantle[:,2]*1000
+    ivs = mantle[:,3]*1000
+    return irad,ivp,ivs
 
-def interp_iasp(prad,irad,vp,vs):
-    fp = scipy.interpolate.interp1d(irad,vp)
-    fs = scipy.interpolate.interp1d(irad,vs)
-    prem_vs = fs(prad)
-    prem_vp = fp(prad)
-    return prem_vp, prem_vs
+def interp_iasp(irad,prad,pmid,pend,prho):
+    fmid = scipy.interpolate.interp1d(prad,pmid,axis=0)
+    fend = scipy.interpolate.interp1d(prad,pend)
+    frho = scipy.interpolate.interp1d(prad,prho)
+    imid = fmid(irad)
+    iend = fend(irad)
+    irho = frho(irad)
+    return imid,iend,irho
 
 def write_model(inarray,title):
     with open(title, 'w') as f:
