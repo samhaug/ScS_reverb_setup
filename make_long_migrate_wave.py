4f5a1f9c31925bdf1f6739211751b5edc9f882be
Samuel Haugland <samhaug@umich.edu>
2017-03-20 14:32:12 -0400

    working make_long_migrate_wave

--- make_long_migrate_wave.py
+++ make_long_migrate_wave.py
@@ -29,10 +29,12 @@ def main():
     st = stream_setup()
     lkup = lkup_setup()
     tr = st[10]
-    #seispy.plot.plot(st[10],phase_list=['ScSScS','ScS^670ScSScS'])
-    master_mask = mask_670(tr,lkup)
-    new_mask = shift_depth(tr,master_mask,480)
-    return new_mask
+    master_mask = mask_670(tr,lkup,plot=False)
+    new_mask = shift_depth(tr,master_mask,368)
+    response_list = shift_discont(tr,new_mask,lkup)
+    for idx,ii in enumerate(response_list[::10]):
+        plt.plot(idx+ii/ii.max(),alpha=0.5,color='k')
+    plt.show()
 
 def lkup_setup():
     lkup_dir = '/home/samhaug/work1/ScS_reverb_sims/lookup_tables/'
@@ -51,10 +53,15 @@ def stream_setup():
        st[idx].stats.sac['o'] += -1468
     return st
 
-def mask_670(tr,lkup):
+def mask_670(tr,lkup,**kwargs):
+    plot = kwargs.get('plot',False)
     evdp = tr.stats.sac['evdp']
     gcarc = tr.stats.sac['gcarc']
     stat = tr.stats.station
+    #mlen is length of wavelet window.
+    mlen = 35
+    #mshi is offset time of window sampler.
+    mshi = 5
 
     # t is for table
     t = lkup[stat]
@@ -63,6 +70,8 @@ def mask_670(tr,lkup):
     dp = sScS2-ScS2
 
     master_mask = {}
+    if plot:
+        plt.plot(tr.data,alpha=0.3,color='k')
     for keys in t:
         if keys[0] == 'b' or keys[0] == 't':
             mask = np.zeros(len(tr.data))
@@ -72,54 +81,60 @@ def mask_670(tr,lkup):
             r = t[keys][i,1]-ScS2+400
             sr = dp+r
             try:
-                mask[r:r+55] += 1.0*tukey(55,0.20)
-                dmask[sr:sr+55] += 1.0*tukey(55,0.20)
+                mask[r+mshi:r+mshi+mlen] += 1.0*tukey(mlen,0.20)
+                dmask[sr+mshi:sr+mshi+mlen] += 1.0*tukey(mlen,0.20)
                 master_mask[keys] = (mask*tr.data)
                 master_mask['d'+keys] = (dmask*tr.data)
+                if plot:
+                    plt.plot(tr.data*dmask,color='b')
+                    plt.plot(tr.data*mask,color='b')
             except ValueError:
                 continue
+    if plot:
+        plt.show()
     return master_mask
 
 def shift_depth(tr,master_mask,depth):
     model = TauPyModel('prem50')
     gcarc = tr.stats.sac['gcarc']
-    evdp = tr.stats.sac['evdp']
+    evdp  = tr.stats.sac['evdp']
     a = model.get_travel_times(source_depth_in_km=evdp,distance_in_degree=gcarc,
                            phase_list=['ScSScS','sScSScS'])
     b = model.get_travel_times(source_depth_in_km=depth,distance_in_degree=gcarc,
                            phase_list=['ScSScS','sScSScS'])
-    shift = int(a[1].time-a[0].time)-int(b[1].time-b[0].time)
-    new_mask = master_mask.copy()
-
-    for keys in new_mask:
+    shift = int(b[1].time-b[0].time)-int(a[1].time-a[0].time)
+    print 'shift time:', shift
+    new_mask = {}
+    for keys in master_mask:
         if keys[0] == 'd':
-            new_mask[keys] = np.roll(new_mask[keys],shift)
+            master_mask[keys]= seispy.data.roll_zero(master_mask[keys],shift)
+    for keys in master_mask:
+        if keys[0] == 'd':
+            continue
+        new_mask[keys] = master_mask[keys]+master_mask['d'+keys]
     return new_mask
 
 def shift_discont(tr,new_mask,lkup):
     stat = tr.stats.station
-    for lkeys in lkup[stat]:
-        ScS2_t = lkup[stat+'/ScS2'][0,1]
-        sScS2_t = lkup[stat+'/sScS2'][0,1]
-        if lkeys == 'ScS2' or lkeys == 'sScS2':
-            continue
-        for mkeys in new_mask:
-            if lkeys in mkeys:
-                i = np.argmin(np.abs(lkup[stat+'/'+lkeys][:,0]-670))
-                d = np.arange(10,2800,2)
-                for ii in d:
-                    ti = np.argmin(np.abs(lkup[stat+'/'+lkeys][:,0]-d))
-                    if mkeys[0] == 'd':
-                        t670 = lkup[stat+'/'+lkeys][i,1]-sScS2_t
-                        td = lkup[stat+'/'+lkeys][i,1]-sScS2_t
-                    else:
-                        t670 = lkup[stat+'/'+lkeys][i,1]-ScS2_t
-                        td = lkup[stat+'/'+lkeys][i,1]-ScS2_t
+    depths = np.arange(10,2800,2)
+    response_list = []
 
-                    new_mask[mkeys]
+    for d in depths:
+        single_response = []
+        for lkeys in lkup[stat]:
+            if lkeys == 'ScS2' or lkeys == 'sScS2':
+                continue
+            i = np.argmin(np.abs(lkup[stat+'/'+lkeys][:,0]-d))
+            td = lkup[stat+'/'+lkeys][i,1]
+            i670 = np.argmin(np.abs(lkup[stat+'/'+lkeys][:,0]-670))
+            t670 = lkup[stat+'/'+lkeys][i670,1]
+            shift = int(td-t670)
+            try:
+                single_response.append(seispy.data.roll_zero(new_mask[lkeys],shift))
+            except KeyError:
+                continue
+        response_list.append(np.sum(single_response,axis=0))
+    response_array = np.array(response_list)
+    return response_array
 
-new_mask = main()
-
-
-
-
+main()
